d0736ee73c50bc4f465a18dbe4c1b81fd10a9c23
VedantSonavane <92395669+VedantSonavane@users.noreply.github.com>
2026-01-11 18:04:27 +0000

    demospace365

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/allspace365/',
+  server: {
+    port: 7281,       // dev server port
+    open: true        // open browser automatically
+  },
+ 
 })
